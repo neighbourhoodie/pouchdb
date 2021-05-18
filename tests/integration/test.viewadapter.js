d3d69347e25313f904bff8f9c6fd9fdaeb291f73
hulkoba <jacoba@neighbourhood.ie>
2021-05-18 14:29:47 +0200

    test(view-adapter): add tests for pouch creation with adapters

--- tests/integration/test.viewadapter.js
+++ tests/integration/test.viewadapter.js
@@ -8,7 +8,6 @@ adapters.forEach(function (adapter) {
 
     var docs = [
       {title : 'abc', value: 1, _id: 'doc1'},
-      {title : 'ghi', value: 3, _id: 'doc3'},
       {title : 'def', value: 2, _id: 'doc2'}
     ];
 
@@ -36,24 +35,50 @@ adapters.forEach(function (adapter) {
     it('Create pouch with separate view adapters', function (done) {
       var db = new PouchDB(dbs.name, {adapter: 'idb', view_adapter: 'memory'});
       db.should.be.an.instanceof(PouchDB);
+
       db.bulkDocs(docs).then(function () {
         db.put(ddoc).then(function () {
           db.query('index', {
             key: 'abc',
             include_docs: true
           }).then(function () {
-            // This is the name of the db where view index data is stored.
-            var viewDbName = Object.keys(localStorage).find(function (key) {
-              return key.startsWith('_pouch_' + db.name + '-mrview-');
+            var savedDbNames = Object.keys(localStorage).filter(function (key) {
+              return key.includes(dbs.name);
             });
-            var request = indexedDB.open(viewDbName, 1);
-            request.onupgradeneeded = function (event) {
+
+            // This is the name of the db where view index data is stored.
+            var viewDbName = savedDbNames.find(function (dbName) {
+              return dbName.includes('-mrview-');
+            });
+
+            // This is the name of the db where documents are stored.
+            var docDbName = savedDbNames.find(function (dbName) {
+              return !dbName.includes('-mrview-');
+            });
+
+            // check indexedDB for saved views
+            // need to add '_pouch_' because views are saved in memory
+            var viewRequest = indexedDB.open('_pouch_' + viewDbName, 1);
+            viewRequest.onupgradeneeded = function (event) {
               // The version of the view database created is 1 which shows that this
               // database was newly created in IndexedDB and did not exist there
               // before. So the view database was created in the database specified in
               // the view_adapter and not in the default `idb`adapter.
               event.oldVersion.should.equal(0);
               event.newVersion.should.equal(1);
+            };
+
+            viewRequest.onsuccess = function () {
+              // Nothing is saved here
+              viewRequest.result.objectStoreNames.length.should.equal(0);
+              viewRequest.result.version.should.equal(1);
+            };
+
+            // check indexedDB for saved docs
+            var docRequest = indexedDB.open(docDbName, 5);
+            docRequest.onsuccess = function () {
+              // something is saved here
+              docRequest.result.objectStoreNames.length.should.equal(7);
               done();
             };
           });
@@ -64,32 +89,44 @@ adapters.forEach(function (adapter) {
     it('Create pouch with no view adapters', function (done) {
       var db = new PouchDB(dbs.name, {adapter: 'idb'});
       db.should.be.an.instanceof(PouchDB);
+
       db.bulkDocs(docs).then(function () {
         db.put(ddoc).then(function () {
           db.query('index', {
             key: 'abc',
             include_docs: true
           }).then(function () {
-            // This is the name of the db where view index data is stored.
-            var viewDbName = Object.keys(localStorage).find(function (key) {
-              return key.startsWith('_pouch_' + db.name + '-mrview-');
-            })
 
-            var request = indexedDB.open(viewDbName, 1);
-            // triggers if the client had no database
-            request.onupgradeneeded = function (event) {
-              // TODO: add comment explaining the process
-              console.log('event.newVersion', event.newVersion);
-              event.oldVersion.should.not.equal(0);
-              event.oldVersion.should.equal(1);
-              event.newVersion.should.not.equal(1);
-              event.newVersion.should.equal(2);
+            var savedDbNames = Object.keys(localStorage).filter(function (key) {
+              return key.includes(dbs.name);
+            });
+
+            // This is the name of the db where view index data is stored.
+            var viewDbName = savedDbNames.find(function (dbName) {
+              return dbName.includes('-mrview-');
+            });
+            // This is the name of the db where documents are stored.
+            var docDbName = savedDbNames.find(function (dbName) {
+              return !dbName.includes('-mrview-');
+            });
+
+            // check indexedDB for saved views
+            var viewRequest = indexedDB.open(viewDbName, 5);
+            viewRequest.onsuccess = function () {
+              // Something is saved here
+              viewRequest.result.objectStoreNames.length.should.equal(7);
+            };
+
+            // check indexedDB for saved docs
+            var docRequest = indexedDB.open(docDbName, 5);
+            docRequest.onsuccess = function () {
+              // something is saved here
+              docRequest.result.objectStoreNames.length.should.equal(7);
               done();
             };
           });
         });
       });
     });
-
   });
 });
