370eb3aaadc08f4134a04dc3b56a094212c4d15a
James Coglan <james@neighbourhood.ie>
2021-08-25 12:36:53 +0100

    Correct path to PouchDB for SERVER=express-pouchdb-minimum

--- tests/misc/express-pouchdb-minimum-for-pouchdb.js
+++ tests/misc/express-pouchdb-minimum-for-pouchdb.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var PouchDB = require('../../');
+var PouchDB = require('../../packages/node_modules/pouchdb');
 
 var app = require('express-pouchdb')(PouchDB, {
   mode: 'minimumForPouchDB'
