07b7474c1d5d3453876f8f703bf79454594c47b3
Jan Lehnardt <jan@apache.org>
2021-09-16 10:44:41 +0200

    wip

--- tests/integration/test.bulk_docs.js
+++ tests/integration/test.bulk_docs.js
@@ -374,9 +374,11 @@ adapters.forEach(function (adapter) {
           }.toString()
         };
 
+        console.log(dbs.name)
         var db = new PouchDB(dbs.name);
-
+        console.log(db)
         db.put(ddoc).then(function () {
+          console.log("after put")
           return db.bulkDocs({
             docs: [
               {
@@ -405,7 +407,14 @@ adapters.forEach(function (adapter) {
               }
             ]
           }, {new_edits: false});
+        }).then(function (res) {
+          console.log("THEN", res)
+          res.should.have.length(1);
+          should.exist(res[0].error);
+          res[0].id.should.equal('doc1');
+          done()
         }).catch(function (res) {
+          console.log("CATCH", res)
           res.should.have.length(1);
           should.exist(res[0].error);
           res[0].id.should.equal('doc1');
