c4f6ff7838a07e4ec524f1473d7f3962dc3df115
Mariano Giménez <marianosgimenez@gmail.com>
2026-03-13 01:09:45 +0100

    refactor: convert to async/await, const and use const/arrow functions

--- tests/find/test-suite-1/test.ddoc.js
+++ tests/find/test-suite-1/test.ddoc.js
@@ -1,9 +1,9 @@
 'use strict';
 
-describe('test.ddoc.js', function () {
-  it.skip('should create an index', function () {
-    var db = context.db;
-    var index = {
+describe('test.ddoc.js', () => {
+  it.skip('should create an index', async () => {
+    const db = context.db;
+    const index = {
       index: {
         fields: ["foo"]
       },
@@ -11,52 +11,49 @@ describe('test.ddoc.js', function () {
       type: "json",
       ddoc: 'foo'
     };
-    return db.createIndex(index).then(function (response) {
-      response.id.should.match(/^_design\//);
-      response.name.should.equal('foo-index');
-      response.result.should.equal('created');
-      return db.createIndex(index);
-    }).then(function (response) {
-      response.id.should.match(/^_design\//);
-      response.name.should.equal('foo-index');
-      response.result.should.equal('exists');
-      return db.getIndexes();
-    }).then(function (resp) {
-      resp.should.deep.equal({
-        "total_rows": 2,
-        "indexes": [
-          {
-            "ddoc": null,
-            "name": "_all_docs",
-            "type": "special",
-            "def": {
-              "fields": [
-                {
-                  "_id": "asc"
-                }
-              ]
-            }
-          },
-          {
-            "ddoc": "_design/foo",
-            "name": "foo-index",
-            "type": "json",
-            "def": {
-              "fields": [
-                {
-                  "foo": "asc"
-                }
-              ]
-            }
+    const createResp = await db.createIndex(index);
+    createResp.id.should.match(/^_design\//);
+    createResp.name.should.equal('foo-index');
+    createResp.result.should.equal('created');
+    const existsResp = await db.createIndex(index);
+    existsResp.id.should.match(/^_design\//);
+    existsResp.name.should.equal('foo-index');
+    existsResp.result.should.equal('exists');
+    const resp = await db.getIndexes();
+    resp.should.deep.equal({
+      "total_rows": 2,
+      "indexes": [
+        {
+          "ddoc": null,
+          "name": "_all_docs",
+          "type": "special",
+          "def": {
+            "fields": [
+              {
+                "_id": "asc"
+              }
+            ]
           }
-        ]
-      });
+        },
+        {
+          "ddoc": "_design/foo",
+          "name": "foo-index",
+          "type": "json",
+          "def": {
+            "fields": [
+              {
+                "foo": "asc"
+              }
+            ]
+          }
+        }
+      ]
     });
   });
 
-  it.skip('should create an index, existing ddoc', function () {
-    var db = context.db;
-    var index = {
+  it.skip('should create an index, existing ddoc', async () => {
+    const db = context.db;
+    const index = {
       index: {
         fields: ["foo"]
       },
@@ -64,57 +61,53 @@ describe('test.ddoc.js', function () {
       type: "json",
       ddoc: 'foo'
     };
-    return db.put({
+    await db.put({
       _id: '_design/foo',
       "language": "query"
-    }).then(function () {
-      return db.createIndex(index);
-    }).then(function (response) {
-      response.id.should.match(/^_design\//);
-      response.name.should.equal('foo-index');
-      response.result.should.equal('created');
-      return db.createIndex(index);
-    }).then(function (response) {
-      response.id.should.match(/^_design\//);
-      response.name.should.equal('foo-index');
-      response.result.should.equal('exists');
-      return db.getIndexes();
-    }).then(function (resp) {
-      resp.should.deep.equal({
-        "total_rows": 2,
-        "indexes": [
-          {
-            "ddoc": null,
-            "name": "_all_docs",
-            "type": "special",
-            "def": {
-              "fields": [
-                {
-                  "_id": "asc"
-                }
-              ]
-            }
-          },
-          {
-            "ddoc": "_design/foo",
-            "name": "foo-index",
-            "type": "json",
-            "def": {
-              "fields": [
-                {
-                  "foo": "asc"
-                }
-              ]
-            }
+    });
+    const createResp = await db.createIndex(index);
+    createResp.id.should.match(/^_design\//);
+    createResp.name.should.equal('foo-index');
+    createResp.result.should.equal('created');
+    const existsResp = await db.createIndex(index);
+    existsResp.id.should.match(/^_design\//);
+    existsResp.name.should.equal('foo-index');
+    existsResp.result.should.equal('exists');
+    const resp = await db.getIndexes();
+    resp.should.deep.equal({
+      "total_rows": 2,
+      "indexes": [
+        {
+          "ddoc": null,
+          "name": "_all_docs",
+          "type": "special",
+          "def": {
+            "fields": [
+              {
+                "_id": "asc"
+              }
+            ]
           }
-        ]
-      });
+        },
+        {
+          "ddoc": "_design/foo",
+          "name": "foo-index",
+          "type": "json",
+          "def": {
+            "fields": [
+              {
+                "foo": "asc"
+              }
+            ]
+          }
+        }
+      ]
     });
   });
 
-  it.skip('should create an index, reused ddoc', function () {
-    var db = context.db;
-    var index = {
+  it.skip('should create an index, reused ddoc', async () => {
+    const db = context.db;
+    const index = {
       index: {
         fields: ["foo"]
       },
@@ -122,82 +115,76 @@ describe('test.ddoc.js', function () {
       type: "json",
       ddoc: 'myddoc'
     };
-    var index2 = {
+    const index2 = {
       index: {
         fields: ['bar']
       },
       name: "bar-index",
       ddoc: 'myddoc'
     };
-    return db.createIndex(index).then(function (response) {
-      response.id.should.match(/^_design\//);
-      response.name.should.equal('foo-index');
-      response.result.should.equal('created');
-      return db.createIndex(index);
-    }).then(function (response) {
-      response.id.should.match(/^_design\//);
-      response.name.should.equal('foo-index');
-      response.result.should.equal('exists');
-      return db.createIndex(index2);
-    }).then(function (response) {
-      response.id.should.match(/^_design\//);
-      response.name.should.equal('bar-index');
-      response.result.should.equal('created');
-      return db.createIndex(index2);
-    }).then(function (response) {
-      response.id.should.match(/^_design\//);
-      response.name.should.equal('bar-index');
-      response.result.should.equal('exists');
-    }).then(function () {
-      return db.getIndexes();
-    }).then(function (resp) {
-      resp.should.deep.equal({
-        "total_rows":3,
-        "indexes": [
-          {
-            "ddoc": null,
-            "name": "_all_docs",
-            "type": "special",
-            "def": {
-              "fields": [
-                {
-                  "_id": "asc"
-                }
-              ]
-            }
-          },
-          {
-            "ddoc": "_design/myddoc",
-            "name": "bar-index",
-            "type": "json",
-            "def": {
-              "fields": [
-                {
-                  "bar": "asc"
-                }
-              ]
-            }
-          },
-          {
-            "ddoc": "_design/myddoc",
-            "name": "foo-index",
-            "type": "json",
-            "def": {
-              "fields": [
-                {
-                  "foo": "asc"
-                }
-              ]
-            }
+    const fooCreateResp = await db.createIndex(index);
+    fooCreateResp.id.should.match(/^_design\//);
+    fooCreateResp.name.should.equal('foo-index');
+    fooCreateResp.result.should.equal('created');
+    const fooExistsResp = await db.createIndex(index);
+    fooExistsResp.id.should.match(/^_design\//);
+    fooExistsResp.name.should.equal('foo-index');
+    fooExistsResp.result.should.equal('exists');
+    const barCreateResp = await db.createIndex(index2);
+    barCreateResp.id.should.match(/^_design\//);
+    barCreateResp.name.should.equal('bar-index');
+    barCreateResp.result.should.equal('created');
+    const barExistsResp = await db.createIndex(index2);
+    barExistsResp.id.should.match(/^_design\//);
+    barExistsResp.name.should.equal('bar-index');
+    barExistsResp.result.should.equal('exists');
+    const resp = await db.getIndexes();
+    resp.should.deep.equal({
+      "total_rows":3,
+      "indexes": [
+        {
+          "ddoc": null,
+          "name": "_all_docs",
+          "type": "special",
+          "def": {
+            "fields": [
+              {
+                "_id": "asc"
+              }
+            ]
           }
-        ]
-      });
+        },
+        {
+          "ddoc": "_design/myddoc",
+          "name": "bar-index",
+          "type": "json",
+          "def": {
+            "fields": [
+              {
+                "bar": "asc"
+              }
+            ]
+          }
+        },
+        {
+          "ddoc": "_design/myddoc",
+          "name": "foo-index",
+          "type": "json",
+          "def": {
+            "fields": [
+              {
+                "foo": "asc"
+              }
+            ]
+          }
+        }
+      ]
     });
   });
 
-  it('Error: invalid ddoc lang', function () {
-    var db = context.db;
-    var index = {
+  it('Error: invalid ddoc lang', async () => {
+    const db = context.db;
+    const index = {
       index: {
         fields: ["foo"]
       },
@@ -205,34 +192,29 @@ describe('test.ddoc.js', function () {
       type: "json",
       ddoc: 'foo'
     };
-    return db.put({
+    await db.put({
       _id: '_design/foo'
-    }).then(function () {
-      return db.createIndex(index);
-    }).then(function () {
-      throw new Error('shouldnt be here');
-    }, function (err) {
-      should.exist(err);
     });
+    try {
+      await db.createIndex(index);
+      throw new Error('shouldnt be here');
+    } catch (err) {
+      should.exist(err);
+    }
   });
 
-  it('handles ddoc with no views and ignores it', function () {
-    var db = context.db;
+  it('handles ddoc with no views and ignores it', async () => {
+    const db = context.db;
 
-    return db.put({
+    await db.put({
       _id: '_design/missing-view',
       language: 'query'
-    })
-    .then(function () {
-      return db.getIndexes();
-    })
-    .then(function (resp) {
-      resp.indexes.length.should.equal(1);
     });
-
+    const resp = await db.getIndexes();
+    resp.indexes.length.should.equal(1);
   });
 
-  it('supports creating many indexes at the same time', function () {
+  it('supports creating many indexes at the same time', async function () {
     // This will time out in IDBNext. This happens because an index creation
     // also queries the view to "warm" it. This triggers our code to decide
     // that the idb handle needs to be dropped and re-created. This is fine if
@@ -245,7 +227,7 @@ describe('test.ddoc.js', function () {
     // same problem) are queued and don't run in parallel.
     this.timeout(5000);
 
-    return Promise.all([
+    await Promise.all([
       context.db.createIndex({index: {fields: ['rank']}}),
       context.db.createIndex({index: {fields: ['series']}}),
       context.db.createIndex({index: {fields: ['debut']}}),
@@ -254,20 +236,19 @@ describe('test.ddoc.js', function () {
       context.db.createIndex({index: {fields: ['name', 'series']}}),
       context.db.createIndex({index: {fields: ['series', 'debut', 'rank']}}),
       context.db.createIndex({index: {fields: ['rank', 'debut']}})
-    ]).then(function () {
-      // At time of writing the createIndex implentation also performs a
-      // search to "warm" the indexes. If this changes to happen async, or not
-      // at all, these queries will force the collision to be tested.
-      return Promise.all([
-        context.db.find({selector: {rank: 'foo'}}),
-        context.db.find({selector: {series: 'foo'}}),
-        context.db.find({selector: {debut: 'foo'}}),
-        context.db.find({selector: {name: 'foo'}}),
-        context.db.find({selector: {name: 'foo', rank: 'foo'}}),
-        context.db.find({selector: {name: 'foo', series: 'foo'}}),
-        context.db.find({selector: {series: 'foo', debut: 'foo', rank: 'foo'}}),
-        context.db.find({selector: {rank: 'foo', debut: 'foo'}}),
-      ]);
-    });
+    ]);
+    // At time of writing the createIndex implentation also performs a
+    // search to "warm" the indexes. If this changes to happen async, or not
+    // at all, these queries will force the collision to be tested.
+    await Promise.all([
+      context.db.find({selector: {rank: 'foo'}}),
+      context.db.find({selector: {series: 'foo'}}),
+      context.db.find({selector: {debut: 'foo'}}),
+      context.db.find({selector: {name: 'foo'}}),
+      context.db.find({selector: {name: 'foo', rank: 'foo'}}),
+      context.db.find({selector: {name: 'foo', series: 'foo'}}),
+      context.db.find({selector: {series: 'foo', debut: 'foo', rank: 'foo'}}),
+      context.db.find({selector: {rank: 'foo', debut: 'foo'}}),
+    ]);
   });
 });
