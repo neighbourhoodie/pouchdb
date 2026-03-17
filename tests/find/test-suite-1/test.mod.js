2ac791790e435f4ce4c41ec593b7a881a0361bb8
Mariano Giménez <marianosgimenez@gmail.com>
2026-03-17 01:35:08 +0100

    refactor: use async/await, const and arrow function

--- tests/find/test-suite-1/test.mod.js
+++ tests/find/test-suite-1/test.mod.js
@@ -1,8 +1,8 @@
 'use strict';
 
-describe('test.mod.js', function () {
-  beforeEach(function () {
-    return context.db.bulkDocs([
+describe('test.mod.js', () => {
+  beforeEach(async () => {
+    await context.db.bulkDocs([
       { name: 'Mario', _id: 'mario', rank: 5, series: 'Mario', debut: 1981, awesome: true },
       { name: 'Jigglypuff', _id: 'puff', rank: 8, series: 'Pokemon', debut: 1996,
         awesome: false },
@@ -22,173 +22,170 @@ describe('test.mod.js', function () {
     ]);
   });
 
-  it('should get all even values', function () {
-    var db = context.db;
-    var index = {
+  it('should get all even values', async () => {
+    const db = context.db;
+    const index = {
       "index": {
         "fields": ["name"]
       }
     };
-    return db.createIndex(index).then(function () {
-      return db.find({
-        selector: {
-          name: {$gte: null},
-          rank: {$mod: [2, 0]}
-        },
-        sort: ['name']
-      }).then(function (resp) {
-        var docs = resp.docs.map(function (doc) {
-          delete doc._rev;
-          return doc;
-        });
-
-        docs.should.deep.equal([
-          { name: 'Captain Falcon', _id: 'falcon', rank: 4, series: 'F-Zero', debut: 1990,
-            awesome: true },
-          { name: 'Jigglypuff', _id: 'puff', rank: 8, series: 'Pokemon', debut: 1996,
-            awesome: false },
-          { name: 'Kirby', _id: 'kirby', series: 'Kirby', rank: 2, debut: 1992, awesome: true },
-          { name: 'Link', rank: 10, _id: 'link', series: 'Zelda', debut: 1986, awesome: true },
-          { name: 'Master Hand', _id: 'master_hand', series: 'Smash Bros', rank: 0, debut: 1999,
-            awesome: false },
-          { name: 'Samus', rank: 12, _id: 'samus', series: 'Metroid', debut: 1986,
-            awesome: true },
-          { name: 'Yoshi', _id: 'yoshi', rank: 6, series: 'Mario', debut: 1990, awesome: true },
-        ]);
-      });
+    await db.createIndex(index);
+    const resp = await db.find({
+      selector: {
+        name: {$gte: null},
+        rank: {$mod: [2, 0]}
+      },
+      sort: ['name']
     });
+    const docs = resp.docs.map((doc) => {
+      delete doc._rev;
+      return doc;
+    });
+
+    docs.should.deep.equal([
+      { name: 'Captain Falcon', _id: 'falcon', rank: 4, series: 'F-Zero', debut: 1990,
+        awesome: true },
+      { name: 'Jigglypuff', _id: 'puff', rank: 8, series: 'Pokemon', debut: 1996,
+        awesome: false },
+      { name: 'Kirby', _id: 'kirby', series: 'Kirby', rank: 2, debut: 1992, awesome: true },
+      { name: 'Link', rank: 10, _id: 'link', series: 'Zelda', debut: 1986, awesome: true },
+      { name: 'Master Hand', _id: 'master_hand', series: 'Smash Bros', rank: 0, debut: 1999,
+        awesome: false },
+      { name: 'Samus', rank: 12, _id: 'samus', series: 'Metroid', debut: 1986,
+        awesome: true },
+      { name: 'Yoshi', _id: 'yoshi', rank: 6, series: 'Mario', debut: 1990, awesome: true },
+    ]);
   });
 
-  it('should error for zero divisor', function () {
-    var db = context.db;
-    var index = {
+  it('should error for zero divisor', async () => {
+    const db = context.db;
+    const index = {
       "index": {
         "fields": ["name"]
       }
     };
-    return db.createIndex(index).then(function () {
-      return db.find({
+    await db.createIndex(index);
+    try {
+      await db.find({
         selector: {
           name: { $gte: null },
           rank: { $mod: [ 0, 0 ] }
         },
         sort: [ 'name' ]
-      }).then(function () {
-        throw new Error('Function should throw');
-      }, function (err) {
-        err.message.should.eq("Query operator $mod's divisor cannot be 0, cannot divide by zero.");
       });
-    });
+      throw new Error('Function should throw');
+    } catch (err) {
+      err.message.should.eq("Query operator $mod's divisor cannot be 0, cannot divide by zero.");
+    }
   });
 
-  it('should error for non-integer divisor', function () {
-    var db = context.db;
-    var index = {
+  it('should error for non-integer divisor', async () => {
+    const db = context.db;
+    const index = {
       "index": {
         "fields": ['name']
       }
     };
-    return db.createIndex(index).then(function () {
-      return db.find({
+    await db.createIndex(index);
+    try {
+      await db.find({
         selector: {
           name: {$gte: null},
           rank: {$mod: ['a', 0]}
         },
         sort: ['name']
-      }).then(function () {
-        throw new Error('Function should throw');
-      }, function (err) {
-        err.message.should.eq("Query operator $mod's divisor is not an integer. Received string: a");
       });
-    });
+      throw new Error('Function should throw');
+    } catch (err) {
+      err.message.should.eq("Query operator $mod's divisor is not an integer. Received string: a");
+    }
   });
 
-  it('should error for non-integer modulus', function () {
-    var db = context.db;
-    var index = {
+  it('should error for non-integer modulus', async () => {
+    const db = context.db;
+    const index = {
       "index": {
         "fields": ["name"]
       }
     };
-    return db.createIndex(index).then(function () {
-      return db.find({
+    await db.createIndex(index);
+    try {
+      await db.find({
         selector: {
           name: {$gte: null},
           rank: {$mod: [1, 'a']}
         },
         sort: ['name']
-      }).then(function () {
-        throw new Error('Function should throw');
-      }, function (err) {
-        err.message.should.eq("Query operator $mod's remainder is not an integer. Received string: a");
       });
-    });
+      throw new Error('Function should throw');
+    } catch (err) {
+      err.message.should.eq("Query operator $mod's remainder is not an integer. Received string: a");
+    }
   });
 
-  it('should error for non-array modulus', function () {
-    var db = context.db;
-    var index = {
+  it('should error for non-array modulus', async () => {
+    const db = context.db;
+    const index = {
       "index": {
         "fields": ["name"]
       }
     };
-    return db.createIndex(index).then(function () {
-      return db.find({
+    await db.createIndex(index);
+    try {
+      await db.find({
         selector: {
           name: {$gte: null},
           rank: {$mod: 'a'}
         },
         sort: ['name']
-      }).then(function () {
-        throw new Error('Function should throw');
-      }, function (err) {
-        err.message.should.eq('Query operator $mod must be an array. Received string: a');
       });
-    });
+      throw new Error('Function should throw');
+    } catch (err) {
+      err.message.should.eq('Query operator $mod must be an array. Received string: a');
+    }
   });
-  it('should error for wrong query value length', function () {
-    var db = context.db;
-    var index = {
+
+  it('should error for wrong query value length', async () => {
+    const db = context.db;
+    const index = {
       "index": {
         "fields": ["name"]
       }
     };
-    return db.createIndex(index).then(function () {
-      return db.find({
+    await db.createIndex(index);
+    try {
+      await db.find({
         selector: {
           name: {$gte: null},
           rank: {$mod: [10]}
         },
         sort: ['name']
-      }).then(function () {
-        throw new Error('Function should throw');
-      }, function (err) {
-        err.message.should.eq('Query operator $mod must be in the format [divisor, remainder], where divisor and remainder are both integers. Received array: ' + JSON.stringify([10], null, "\t"));
       });
-    });
+      throw new Error('Function should throw');
+    } catch (err) {
+      err.message.should.eq('Query operator $mod must be in the format [divisor, remainder], where divisor and remainder are both integers. Received array: ' + JSON.stringify([10], null, "\t"));
+    }
   });
 
-  it('should ignore non-int field values', function () {
-    var db = context.db;
-    return context.db.bulkDocs([
+  it('should ignore non-int field values', async () => {
+    const db = context.db;
+    await context.db.bulkDocs([
       { _id: "int", int: 10 },
       { _id: "float", int: 10.1 },
       { _id: "string", int: "10" },
-    ]).then(function () {
-      return db.find({
-        selector: {
-          int: { $mod: [ 3, 1 ] }
-        },
-      }).then(function (resp) {
-        var docs = resp.docs.map(function (doc) {
-          delete doc._rev;
-          return doc;
-        });
-
-        docs.should.deep.equal([
-          { _id: "int", int: 10 }
-        ]);
-      });
+    ]);
+    const resp = await db.find({
+      selector: {
+        int: { $mod: [ 3, 1 ] }
+      },
     });
+    const docs = resp.docs.map((doc) => {
+      delete doc._rev;
+      return doc;
+    });
+
+    docs.should.deep.equal([
+      { _id: "int", int: 10 }
+    ]);
   });
 });
