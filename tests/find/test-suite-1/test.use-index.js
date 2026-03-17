7a3896f1de1114172d734483c1d7401974854477
Mariano Giménez <marianosgimenez@gmail.com>
2026-03-17 02:50:09 +0100

    refactor: use async/await, const and arrow function

--- tests/find/test-suite-1/test.use-index.js
+++ tests/find/test-suite-1/test.use-index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-describe('test.use-index.js', function () {
-  beforeEach(function () {
-    var db = context.db;
-    return db.bulkDocs([
+describe('test.use-index.js', () => {
+  beforeEach(async () => {
+    const db = context.db;
+    await db.bulkDocs([
       { name: 'mario', _id: 'mario', rank: 5, series: 'mario', debut: 1981 },
       { name: 'jigglypuff', _id: 'puff', rank: 8, series: 'pokemon', debut: 1996 },
       { name: 'link', rank: 10, _id: 'link', series: 'zelda', debut: 1986 },
@@ -16,95 +16,87 @@ describe('test.use-index.js', function () {
       { name: 'samus', rank: 12, _id: 'samus', series: 'metroid', debut: 1986 },
       { name: 'yoshi', _id: 'yoshi', rank: 6, series: 'mario', debut: 1990 },
       { name: 'kirby', _id: 'kirby', series: 'kirby', rank: 2, debut: 1992 }
-    ])
-    .then(function () {
-      return db.createIndex({
-        index: {
-          "fields": ["name", "series"]
-        },
-        "ddoc": "index-1",
-        "type": "json"
-      });
-    })
-    .then(function () {
-      return db.createIndex({
-        index: {
-          "fields": ["name", "debut"]
-        },
-        "ddoc": "index-2",
-        "type": "json"
-      });
-    })
-    .then(function () {
-      return db.createIndex({
-        index: {
-          "fields": ["name", "another_field"]
-        },
-        "ddoc": "index-3",
-        "name": "third-index",
-        "type": "json"
-      });
+    ]);
+    await db.createIndex({
+      index: {
+        "fields": ["name", "series"]
+      },
+      "ddoc": "index-1",
+      "type": "json"
+    });
+    await db.createIndex({
+      index: {
+        "fields": ["name", "debut"]
+      },
+      "ddoc": "index-2",
+      "type": "json"
+    });
+    await db.createIndex({
+      index: {
+        "fields": ["name", "another_field"]
+      },
+      "ddoc": "index-3",
+      "name": "third-index",
+      "type": "json"
     });
   });
 
-  it.skip('use index based on ddoc', function () {
-    var db = context.db;
-    return db.explain({
+  it.skip('use index based on ddoc', async () => {
+    const db = context.db;
+    const resp = await db.explain({
       selector: {
         name: 'mario'
       },
       use_index: "index-2",
       fields: ["_id"]
-    }).then(function (resp) {
-      resp.index.ddoc.should.equal("_design/index-2");
     });
+    resp.index.ddoc.should.equal("_design/index-2");
   });
 
   if (testUtils.adapterType() === 'http') {
     return;
   }
-  it('use index based on ddoc and name', function () {
-    var db = context.db;
-    return db.explain({
+  it('use index based on ddoc and name', async () => {
+    const db = context.db;
+    const resp = await db.explain({
       selector: {
         name: 'mario'
       },
       use_index: ["index-3", "third-index"],
       fields: ["_id"]
-    }).then(function (resp) {
-      resp.index.ddoc.should.equal("_design/index-3");
     });
+    resp.index.ddoc.should.equal("_design/index-3");
   });
 
-  it('throws error if index does not exist', function () {
-    var db = context.db;
-    return db.explain({
-      selector: {
-        name: 'mario'
-      },
-      use_index: "index-not-found",
-      fields: ["_id"]
-    }).then(function () {
+  it('throws error if index does not exist', async () => {
+    const db = context.db;
+    try {
+      await db.explain({
+        selector: {
+          name: 'mario'
+        },
+        use_index: "index-not-found",
+        fields: ["_id"]
+      });
       throw "Should not get here";
-    })
-    .catch(function (err) {
+    } catch (err) {
       err.error.should.equal("unknown_error");
-    });
+    }
   });
 
-  it('throws error if index cannot be used', function () {
-    var db = context.db;
-    return db.explain({
-      selector: {
-        rank : 2
-      },
-      use_index: "index-2",
-      fields: ["_id"]
-    }).then(function () {
+  it('throws error if index cannot be used', async () => {
+    const db = context.db;
+    try {
+      await db.explain({
+        selector: {
+          rank : 2
+        },
+        use_index: "index-2",
+        fields: ["_id"]
+      });
       throw "Should not get here";
-    })
-    .catch(function (err) {
+    } catch (err) {
       err.error.should.equal("no_usable_index");
-    });
+    }
   });
 });
