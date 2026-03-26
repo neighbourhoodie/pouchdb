c833151bc5607b58cc26381c903365e691db12aa
Mariano Giménez <marianosgimenez@gmail.com>
2026-03-26 19:28:07 +0100

    refactor: use destructuring const and arrow function

--- tests/integration/node.setup.js
+++ tests/integration/node.setup.js
@@ -2,24 +2,24 @@
 // throw an error if any EventEmitter adds too many listeners
 require('throw-max-listeners-error');
 
-var seedrandom = require('seedrandom');
-var seed = (process.env.SEED || Date.now()) + "";
-console.log('Seeded with: ' + seed);
+const seedrandom = require('seedrandom');
+const seed = (process.env.SEED || Date.now()) + "";
+console.log(`Seeded with: ${seed}`);
 seedrandom(seed, { global: true });
 
-var testsDir = process.env.TESTS_DIR || './tmp';
-var exec = require('child_process').exec;
+const testsDir = process.env.TESTS_DIR || './tmp';
+const { exec } = require('child_process');
 function cleanup() {
   // Remove test databases
-  exec('rm -r ' + testsDir);
+  exec(`rm -r ${testsDir}`);
 }
-exec('mkdir -p ' + testsDir, function () {
+exec(`mkdir -p ${testsDir}`, () => {
   process.on('SIGINT', cleanup);
   process.on('exit', cleanup);
 });
 global.testUtils = require('./utils.js');
 global.PouchDB = testUtils.loadPouchDB();
-var chai = require('chai');
+const chai = require('chai');
 chai.use(require('chai-as-promised'));
 global.should = chai.should();
 global.assert = chai.assert;
