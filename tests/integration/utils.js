bf8737427d31d1a4c82f3ce3061e004015fa6b6d
Mariano Giménez <marianosgimenez@gmail.com>
2026-04-02 15:37:51 +0200

    refactor: use async/await, const and arrow function

--- tests/integration/utils.js
+++ tests/integration/utils.js
@@ -1,47 +1,46 @@
 'use strict';
 
-var testUtils = Object.create(require('../common-utils'));
+const testUtils = Object.create(require('../common-utils'));
 
-function uniq(list) {
-  var map = {};
-  list.forEach(function (item) {
+const uniq = (list) => {
+  const map = {};
+  list.forEach((item) => {
     map[item] = true;
   });
   return Object.keys(map);
-}
+};
 
-testUtils.isCouchMaster = function () {
+testUtils.isCouchMaster = () => {
   return 'SERVER' in testUtils.params() &&
     testUtils.params().SERVER === 'couchdb-master';
 };
 
-testUtils.isChrome = function () {
+testUtils.isChrome = () => {
   return (typeof window !== 'undefined') && window.navigator &&
       /Google Inc/.test(window.navigator.vendor);
 };
 
-testUtils.isSafari = function () {
+testUtils.isSafari = () => {
   return (typeof process === 'undefined' || process.browser) &&
       /Safari/.test(window.navigator.userAgent) &&
       !/Chrome/.test(window.navigator.userAgent);
 };
 
-testUtils.adapterType = function () {
+testUtils.adapterType = () => {
   return testUtils.adapters().indexOf('http') < 0 ? 'local' : 'http';
 };
 
-testUtils.readBlob = function (blob, callback) {
+testUtils.readBlob = (blob, callback) => {
   if (testUtils.isNode()) {
     callback(blob.toString('binary'));
   } else {
-    var reader = new FileReader();
-    reader.onloadend = function () {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      let binary = "";
+      const bytes = new Uint8Array(reader.result || '');
+      const length = bytes.byteLength;
 
-      var binary = "";
-      var bytes = new Uint8Array(this.result || '');
-      var length = bytes.byteLength;
-
-      for (var i = 0; i < length; i++) {
+      for (let i = 0; i < length; i++) {
         binary += String.fromCharCode(bytes[i]);
       }
 
@@ -51,17 +50,17 @@ testUtils.readBlob = function (blob, callback) {
   }
 };
 
-testUtils.readBlobPromise = function (blob) {
-  return new Promise(function (resolve) {
+testUtils.readBlobPromise = (blob) => {
+  return new Promise((resolve) => {
     testUtils.readBlob(blob, resolve);
   });
 };
 
-testUtils.base64Blob = function (blob, callback) {
+testUtils.base64Blob = (blob, callback) => {
   if (testUtils.isNode()) {
     callback(blob.toString('base64'));
   } else {
-    testUtils.readBlob(blob, function (binary) {
+    testUtils.readBlob(blob, (binary) => {
       callback(testUtils.btoa(binary));
     });
   }
@@ -69,29 +68,29 @@ testUtils.base64Blob = function (blob, callback) {
 
 // Prefix http adapter database names with their host and
 // node adapter ones with a db location
-testUtils.adapterUrl = function (adapter, name) {
+testUtils.adapterUrl = (adapter, name) => {
 
   // CouchDB master has problems with cycling databases rapidly
   // so give tests separate names
-  name += '_' + Date.now();
+  name += `_${Date.now()}`;
 
   if (adapter === 'http') {
-    return testUtils.couchHost() + '/' + name;
+    return `${testUtils.couchHost()}/${name}`;
   }
   return name;
 };
 
 // Delete specified databases
-testUtils.cleanup = function (dbs, done) {
+testUtils.cleanup = (dbs, done) => {
   dbs = uniq(dbs);
-  var num = dbs.length;
-  var finished = function () {
+  let num = dbs.length;
+  const finished = () => {
     if (--num === 0) {
       done();
     }
   };
 
-  dbs.forEach(function (db) {
+  dbs.forEach((db) => {
     new PouchDB(db).destroy(finished, finished);
   });
 };
@@ -99,8 +98,8 @@ testUtils.cleanup = function (dbs, done) {
 // Put doc after prevRev (so that doc is a child of prevDoc
 // in rev_tree). Doc must have _rev. If prevRev is not specified
 // just insert doc with correct _rev (new_edits=false!)
-testUtils.putAfter = function (db, doc, prevRev, callback) {
-  var newDoc = testUtils.assign({}, doc);
+testUtils.putAfter = (db, doc, prevRev, callback) => {
+  const newDoc = testUtils.assign({}, doc);
   if (!prevRev) {
     db.put(newDoc, { new_edits: false }, callback);
     return;
@@ -117,62 +116,60 @@ testUtils.putAfter = function (db, doc, prevRev, callback) {
 
 // docs will be inserted one after another
 // starting from root
-testUtils.putBranch = function (db, docs, callback) {
-  function insert(i) {
-    var doc = docs[i];
-    var prev = i > 0 ? docs[i - 1]._rev : null;
-    function next() {
+testUtils.putBranch = (db, docs, callback) => {
+  const insert = (i) => {
+    const doc = docs[i];
+    const prev = i > 0 ? docs[i - 1]._rev : null;
+    const next = () => {
       if (i < docs.length - 1) {
         insert(i + 1);
       } else {
         callback();
       }
-    }
-    db.get(doc._id, { rev: doc._rev }, function (err) {
+    };
+    db.get(doc._id, { rev: doc._rev }, (err) => {
       if (err) {
-        testUtils.putAfter(db, docs[i], prev, function () {
+        testUtils.putAfter(db, docs[i], prev, () => {
           next();
         });
       } else {
         next();
       }
     });
-  }
+  };
   insert(0);
 };
 
-testUtils.putTree = function (db, tree, callback) {
-  function insert(i) {
-    var branch = tree[i];
-    testUtils.putBranch(db, branch, function () {
+testUtils.putTree = (db, tree, callback) => {
+  const insert = (i) => {
+    const branch = tree[i];
+    testUtils.putBranch(db, branch, () => {
       if (i < tree.length - 1) {
         insert(i + 1);
       } else {
         callback();
       }
     });
-  }
+  };
   insert(0);
 };
 
-function parseHostWithCreds(host) {
-  var { origin, pathname, username, password } = new URL(host);
-  var url = `${origin}${pathname}`;
-  var options = {};
+const parseHostWithCreds = (host) => {
+  const { origin, pathname, username, password } = new URL(host);
+  const url = `${origin}${pathname}`;
+  const options = {};
   if (username || password) {
     options.headers = {};
-    options.headers['Authorization'] = 'Basic: ' + testUtils.btoa(`${username}:${password}`);
+    options.headers['Authorization'] = `Basic: ${testUtils.btoa(`${username}:${password}`)}`;
   }
   return { url, options };
-}
+};
 
-testUtils.isCouchDB = function (cb) {
-  var {url, options} = parseHostWithCreds(testUtils.couchHost());
-  PouchDB.fetch(url, options).then(function (response) {
-    return response.json();
-  }).then(function (res) {
-    cb('couchdb' in res || 'express-pouchdb' in res);
-  });
+testUtils.isCouchDB = async (cb) => {
+  const {url, options} = parseHostWithCreds(testUtils.couchHost());
+  const response = await PouchDB.fetch(url, options);
+  const res = await response.json();
+  cb('couchdb' in res || 'express-pouchdb' in res);
 };
 
 testUtils.getServerType = async () => {
@@ -195,23 +192,23 @@ testUtils.getServerType = async () => {
   throw new Error(`Could not find a known server type in response: ${JSON.stringify(res)}`);
 };
 
-testUtils.writeDocs = function (db, docs, callback, res) {
+testUtils.writeDocs = (db, docs, callback, res) => {
   if (!res) {
     res = [];
   }
   if (!docs.length) {
     return callback(null, res);
   }
-  var doc = docs.shift();
-  db.put(doc, function (err, info) {
+  const doc = docs.shift();
+  db.put(doc, (err, info) => {
     res.push(info);
     testUtils.writeDocs(db, docs, callback, res);
   });
 };
 
 // Borrowed from: http://stackoverflow.com/a/840849
-testUtils.eliminateDuplicates = function (arr) {
-  var i, element, len = arr.length, out = [], obj = {};
+testUtils.eliminateDuplicates = (arr) => {
+  let i, element, len = arr.length, out = [], obj = {};
   for (i = 0; i < len; i++) {
     obj[arr[i]] = 0;
   }
@@ -224,34 +221,31 @@ testUtils.eliminateDuplicates = function (arr) {
 };
 
 // Promise finally util similar to Q.finally
-testUtils.fin = function (promise, cb) {
-  return promise.then(function (res) {
-    var promise2 = cb();
+testUtils.fin = async (promise, cb) => {
+  try {
+    const res = await promise;
+    const promise2 = cb();
     if (typeof promise2.then === 'function') {
-      return promise2.then(function () {
-        return res;
-      });
+      await promise2;
     }
     return res;
-  }, function (reason) {
-    var promise2 = cb();
+  } catch (reason) {
+    const promise2 = cb();
     if (typeof promise2.then === 'function') {
-      return promise2.then(function () {
-        throw reason;
-      });
+      await promise2;
     }
     throw reason;
-  });
+  }
 };
 
-testUtils.promisify = function (fun, context) {
+testUtils.promisify = (fun, context) => {
   return function () {
-    var args = [];
-    for (var i = 0; i < arguments.length; i++) {
+    const args = [];
+    for (let i = 0; i < arguments.length; i++) {
       args[i] = arguments[i];
     }
-    return new Promise(function (resolve, reject) {
-      args.push(function (err, res) {
+    return new Promise((resolve, reject) => {
+      args.push((err, res) => {
         if (err) {
           return reject(err);
         }
@@ -265,8 +259,8 @@ testUtils.promisify = function (fun, context) {
 // We need to use pouchdb-for-coverage here to ensure that e.g pouchdb-utils
 // and pouchdb-ajax don't get pulled in, because then our coverage tests
 // would complain that we're not using the "whole" thing.
-var PouchForCoverage = require('../../packages/node_modules/pouchdb-for-coverage');
-var pouchUtils = PouchForCoverage.utils;
+const PouchForCoverage = require('../../packages/node_modules/pouchdb-for-coverage');
+const pouchUtils = PouchForCoverage.utils;
 testUtils.binaryStringToBlob = pouchUtils.binaryStringToBlobOrBuffer;
 testUtils.btoa = pouchUtils.btoa;
 testUtils.atob = pouchUtils.atob;
@@ -277,7 +271,7 @@ testUtils.errors = PouchForCoverage.Errors;
 testUtils.assign = pouchUtils.assign;
 testUtils.generateReplicationId = pouchUtils.generateReplicationId;
 
-testUtils.makeBlob = function (data, type) {
+testUtils.makeBlob = (data, type) => {
   if (testUtils.isNode()) {
     // "global.Buffer" is to avoid Browserify pulling this in
     return global.Buffer.from(data, 'binary');
@@ -288,12 +282,12 @@ testUtils.makeBlob = function (data, type) {
   }
 };
 
-testUtils.getUnHandledRejectionEventName = function () {
+testUtils.getUnHandledRejectionEventName = () => {
   return typeof window !== 'undefined' ? 'unhandledrejection' :
     'unhandledRejection';
 };
 
-testUtils.addGlobalEventListener = function (eventName, listener) {
+testUtils.addGlobalEventListener = (eventName, listener) => {
   // The window test has to go first because the process test will pass
   // in the browser's test environment
   if (typeof window !== 'undefined' && window.addEventListener) {
@@ -307,12 +301,12 @@ testUtils.addGlobalEventListener = function (eventName, listener) {
   return null;
 };
 
-testUtils.addUnhandledRejectionListener = function (listener) {
+testUtils.addUnhandledRejectionListener = (listener) => {
   return testUtils.addGlobalEventListener(
     testUtils.getUnHandledRejectionEventName(), listener);
 };
 
-testUtils.removeGlobalEventListener = function (eventName, listener) {
+testUtils.removeGlobalEventListener = (eventName, listener) => {
   if (typeof process !== 'undefined') {
     return process.removeListener(eventName, listener);
   }
@@ -324,12 +318,12 @@ testUtils.removeGlobalEventListener = function (eventName, listener) {
   return null;
 };
 
-testUtils.removeUnhandledRejectionListener = function (listener) {
+testUtils.removeUnhandledRejectionListener = (listener) => {
   return testUtils.removeGlobalEventListener(
     testUtils.getUnHandledRejectionEventName(), listener);
 };
 
-testUtils.sortById = function (a, b) {
+testUtils.sortById = (a, b) => {
   return a._id < b._id ? -1 : 1;
 };
 
