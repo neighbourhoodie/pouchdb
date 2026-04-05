2682efb4c0402ce982a2f039e154ec7e913c6bd5
Mariano Giménez <marianosgimenez@gmail.com>
2026-04-05 21:54:50 +0200

    refactor: use async/await, const and arrow function

--- tests/integration/test.replicationBackoff.js
+++ tests/integration/test.replicationBackoff.js
@@ -1,41 +1,41 @@
 'use strict';
 
-var adapters = [
+const adapters = [
   ['local', 'http']
 ];
 
-adapters.forEach(function (adapters) {
-  var suiteName = 'test.replicationBackoff.js-' + adapters[0] + '-' + adapters[1];
-  describe(suiteName, function () {
+adapters.forEach((adapters) => {
+  const suiteName = `test.replicationBackoff.js-${adapters[0]}-${adapters[1]}`;
+  describe(suiteName, () => {
 
-    var dbs = {};
+    const dbs = {};
 
-    beforeEach(function () {
+    beforeEach(() => {
       dbs.name = testUtils.adapterUrl(adapters[0], 'testdb');
       dbs.remote = testUtils.adapterUrl(adapters[1], 'test_repl_remote');
     });
 
-    afterEach(function (done) {
-      testUtils.cleanup([dbs.name, dbs.remote], done);
+    afterEach(async () => {
+      await new Promise(resolve => testUtils.cleanup([dbs.name, dbs.remote], resolve));
     });
 
-    it('Issue 5402 should not keep adding event listeners when backoff is firing', function (done) {
+    it('Issue 5402 should not keep adding event listeners when backoff is firing', async function () {
       this.timeout(1500);
-      var remote = new PouchDB(dbs.remote, {
-        fetch: function () {
+      const remote = new PouchDB(dbs.remote, {
+        fetch: () => {
           throw new Error('flunking you');
         }
       });
-      var db = new PouchDB(dbs.name);
-      var backOffCount = 0;
-      var numberOfActiveListeners = 0;
+      const db = new PouchDB(dbs.name);
+      let backOffCount = 0;
+      let numberOfActiveListeners = 0;
 
-      var replication = db.sync(remote, {
+      const replication = db.sync(remote, {
         live: true,
         retry: true,
         heartbeat: 1,
         timeout: 1,
-        back_off_function: function () {
+        back_off_function: () => {
           numberOfActiveListeners = replication.pull.listeners("active").length;
           ++backOffCount;
           if (backOffCount > 15 || numberOfActiveListeners > 3) {
@@ -45,12 +45,15 @@ adapters.forEach(function (adapters) {
         }
       });
 
-      replication.on("complete", function () {
-        if (numberOfActiveListeners > 3) {
-          done(new Error("Number of 'active' listeners shouldn't grow larger than one.  Currently at " + numberOfActiveListeners));
-        } else {
-          done();
-        }
+      await new Promise((resolve, reject) => {
+        replication.on("complete", () => {
+          if (numberOfActiveListeners > 3) {
+            reject(new Error(`Number of 'active' listeners shouldn't grow larger than one.
+              Currently at ${numberOfActiveListeners}`));
+          } else {
+            resolve();
+          }
+        });
       });
     });
   });
