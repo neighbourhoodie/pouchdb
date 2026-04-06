2f7dc746875747f000b78f7d991678e0117d937d
Mariano Giménez <marianosgimenez@gmail.com>
2026-04-07 00:15:07 +0200

    refactor: use async/await, const and arrow function

--- tests/integration/test.taskqueue.js
+++ tests/integration/test.taskqueue.js
@@ -1,52 +1,51 @@
 'use strict';
 
-var adapters = ['http', 'local'];
+const adapters = ['http', 'local'];
 
-adapters.forEach(function (adapter) {
-  describe('test.taskqueue.js-' + adapter, function () {
+adapters.forEach((adapter) => {
+  describe(`test.taskqueue.js-${adapter}`, () => {
 
-    var dbs = {};
+    const dbs = {};
 
-    beforeEach(function () {
+    beforeEach(() => {
       dbs.name = testUtils.adapterUrl(adapter, 'testdb');
     });
 
-    afterEach(function (done) {
+    afterEach((done) => {
       testUtils.cleanup([dbs.name], done);
     });
 
-    it('Add a doc', function () {
-      var db = new PouchDB(dbs.name);
+    it('Add a doc', () => {
+      const db = new PouchDB(dbs.name);
       return db.post({test: 'somestuff'});
     });
 
-    it('Bulk docs', function () {
-      var db = new PouchDB(dbs.name);
-      return db.bulkDocs({
+    it('Bulk docs', async () => {
+      const db = new PouchDB(dbs.name);
+      const infos = await db.bulkDocs({
         docs: [
           { test: 'somestuff' },
           { test: 'another' }
         ]
-      }).then(function (infos) {
-        should.not.exist(infos[0].error);
-        should.not.exist(infos[1].error);
       });
+      should.not.exist(infos[0].error);
+      should.not.exist(infos[1].error);
     });
 
-    it('Get', function () {
-      var db = new PouchDB(dbs.name);
-      return db.get('0').then(function () {
+    it('Get', async () => {
+      const db = new PouchDB(dbs.name);
+      try {
+        await db.get('0');
         throw 'Get should error';
-      }).catch(function (err) {
+      } catch (err) {
         should.exist(err);
-      });
+      }
     });
 
-    it('Info', function () {
-      var db = new PouchDB(dbs.name);
-      return db.info().then(function (info) {
-        info.doc_count.should.equal(0);
-      });
+    it('Info', async () => {
+      const db = new PouchDB(dbs.name);
+      const info = await db.info();
+      info.doc_count.should.equal(0);
     });
 
   });
