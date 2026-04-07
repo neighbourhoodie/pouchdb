eac6221bd55b756ed286cc0a869929df78672bbd
Mariano Giménez <marianosgimenez@gmail.com>
2026-04-07 09:35:56 +0200

    refactor: use async/await, const and arrow function

--- tests/integration/test.slash_id.js
+++ tests/integration/test.slash_id.js
@@ -1,55 +1,47 @@
 'use strict';
 
-var adapters = ['local', 'http'];
-var repl_adapters = [
+const adapters = ['local', 'http'];
+const repl_adapters = [
   ['local', 'http'],
   ['http', 'http'],
   ['http', 'local'],
   ['local', 'local']
 ];
 
-adapters.forEach(function (adapter) {
-  describe('test.slash_ids.js-' + adapter, function () {
+adapters.forEach((adapter) => {
+  describe(`test.slash_ids.js-${adapter}`, () => {
 
-    var dbs = {};
+    const dbs = {};
 
-    beforeEach(function () {
+    beforeEach(() => {
       dbs.name = testUtils.adapterUrl(adapter, 'testdb');
     });
 
-    afterEach(function (done) {
+    afterEach((done) => {
       testUtils.cleanup([dbs.name], done);
     });
 
-    it('Insert a doc, putAttachment and get', function (done) {
-      var db = new PouchDB(dbs.name);
-      var docId = 'doc/with/slashes';
-      var attachmentId = 'attachment/with/slashes';
-      var blobData = 'attachment content';
-      var blob = testUtils.makeBlob(blobData);
-      var doc = {_id: docId, test: true};
-      db.put(doc, function (err, info) {
-        should.not.exist(err, 'saved doc');
-        info.id.should.equal('doc/with/slashes', 'id is the same as inserted');
-        db.putAttachment(docId, attachmentId, info.rev, blob, 'text/plain',
-                         function () {
-          db.getAttachment(docId, attachmentId, function (err, res) {
-            testUtils.readBlob(res, function (data) {
-              data.should.equal(blobData);
-              db.get(docId, function (err, res) {
-                res._id.should.equal(docId);
-                res._attachments.should.include.keys(attachmentId);
-                done();
-              });
-            });
-          });
-        });
-      });
+    it('Insert a doc, putAttachment and get', async () => {
+      const db = new PouchDB(dbs.name);
+      const docId = 'doc/with/slashes';
+      const attachmentId = 'attachment/with/slashes';
+      const blobData = 'attachment content';
+      const blob = testUtils.makeBlob(blobData);
+      const doc = {_id: docId, test: true};
+      const info = await db.put(doc);
+      info.id.should.equal('doc/with/slashes', 'id is the same as inserted');
+      await db.putAttachment(docId, attachmentId, info.rev, blob, 'text/plain');
+      const res = await db.getAttachment(docId, attachmentId);
+      const data = await testUtils.readBlobPromise(res);
+      data.should.equal(blobData);
+      const getRes = await db.get(docId);
+      getRes._id.should.equal(docId);
+      getRes._attachments.should.include.keys(attachmentId);
     });
 
-    it('BulkDocs and changes', function (done) {
-      var db = new PouchDB(dbs.name);
-      var docs = [
+    it('BulkDocs and changes', async () => {
+      const db = new PouchDB(dbs.name);
+      const docs = [
         {_id: 'part/doc1', int: 1},
         {_id: 'part/doc2', int: 2, _attachments: {
           'attachment/with/slash': {
@@ -59,60 +51,53 @@ adapters.forEach(function (adapter) {
         }},
         {_id: 'part/doc3', int: 3}
       ];
-      db.bulkDocs({ docs }, function (err, res) {
-        for (var i = 0; i < 3; i++) {
-          res[i].ok.should.equal(true, 'correctly inserted ' + docs[i]._id);
-        }
-        db.allDocs({
-          include_docs: true,
-          attachments: true
-        }, function (err, res) {
-          res.rows.sort(function (a, b) {
-            return a.doc.int - b.doc.int;
-          });
-          for (var i = 0; i < 3; i++) {
-            res.rows[i].doc._id.should
-              .equal(docs[i]._id, '(allDocs) correctly inserted ' +
-                     docs[i]._id);
-          }
-          res.rows[1].doc._attachments.should.include
-            .keys('attachment/with/slash');
-          db.changes({return_docs: true}).on('complete', function (res) {
-            res.results.sort(function (a, b) {
-              return a.id.localeCompare(b.id);
-            });
-            for (var i = 0; i < 3; i++) {
-              res.results[i].id.should
-                .equal(docs[i]._id, 'correctly inserted');
-            }
-            done();
-          }).on('error', done);
-        });
+      const res = await db.bulkDocs({ docs });
+      for (let i = 0; i < 3; i++) {
+        res[i].ok.should.equal(true, `correctly inserted ${docs[i]._id}`);
+      }
+      const allDocsRes = await db.allDocs({
+        include_docs: true,
+        attachments: true
       });
+      allDocsRes.rows.sort((a, b) => a.doc.int - b.doc.int);
+      for (let i = 0; i < 3; i++) {
+        allDocsRes.rows[i].doc._id.should
+          .equal(docs[i]._id, `(allDocs) correctly inserted ${docs[i]._id}`);
+      }
+      allDocsRes.rows[1].doc._attachments.should.include
+        .keys('attachment/with/slash');
+      const changesRes = await new Promise((resolve, reject) => {
+        db.changes({return_docs: true}).on('complete', resolve).on('error', reject);
+      });
+      changesRes.results.sort((a, b) => a.id.localeCompare(b.id));
+      for (let i = 0; i < 3; i++) {
+        changesRes.results[i].id.should
+          .equal(docs[i]._id, 'correctly inserted');
+      }
     });
 
   });
 });
 
 
-repl_adapters.forEach(function (adapters) {
-  describe('test.slash_ids.js-' + adapters[0] + '-' + adapters[1], function () {
+repl_adapters.forEach((adapters) => {
+  describe(`test.slash_ids.js-${adapters[0]}-${adapters[1]}`, () => {
 
-    var dbs = {};
+    const dbs = {};
 
-    beforeEach(function (done) {
+    beforeEach((done) => {
       dbs.name = testUtils.adapterUrl(adapters[0], 'test_slash_ids');
       dbs.remote = testUtils.adapterUrl(adapters[1], 'test_slash_ids_remote');
       testUtils.cleanup([dbs.name, dbs.remote], done);
     });
 
-    afterEach(function (done) {
+    afterEach((done) => {
       testUtils.cleanup([dbs.name, dbs.remote], done);
     });
 
 
-    it('Attachments replicate', function (done) {
-      var binAttDoc = {
+    it('Attachments replicate', async () => {
+      const binAttDoc = {
         _id: 'bin_doc/with/slash',
         _attachments: {
           'foo/with/slash.txt': {
@@ -121,25 +106,20 @@ repl_adapters.forEach(function (adapters) {
           }
         }
       };
-      var docs1 = [
+      const docs1 = [
         binAttDoc,
         {_id: '0', integer: 0},
         {_id: '1', integer: 1},
         {_id: '2', integer: 2},
         {_id: '3', integer: 3}
       ];
-      var db = new PouchDB(dbs.name);
-      var remote = new PouchDB(dbs.remote);
-      remote.bulkDocs({ docs: docs1 }, function () {
-        db.replicate.from(remote, function () {
-          db.get('bin_doc/with/slash', { attachments: true },
-            function (err, doc) {
-            binAttDoc._attachments['foo/with/slash.txt'].data.should
-              .equal(doc._attachments['foo/with/slash.txt'].data);
-            done();
-          });
-        });
-      });
+      const db = new PouchDB(dbs.name);
+      const remote = new PouchDB(dbs.remote);
+      await remote.bulkDocs({ docs: docs1 });
+      await db.replicate.from(remote);
+      const doc = await db.get('bin_doc/with/slash', { attachments: true });
+      binAttDoc._attachments['foo/with/slash.txt'].data.should
+        .equal(doc._attachments['foo/with/slash.txt'].data);
     });
   });
 });
