eda846f9848633247533735c7b43ff8d1adac3ee
Alex Feyerke <alex@neighbourhood.ie>
2026-05-04 11:16:46 +0200

    fix: fixed, updated and simplified the build-site script
    No longer maxes out CPUs and no more orphaned node processes

--- bin/build-site.js
+++ bin/build-site.js
@@ -6,53 +6,18 @@ const { promisify } = require('node:util');
 const exec = promisify(require('node:child_process').exec);
 
 var fs = require('fs');
-const Path = require('node:path');
 
-var replace = require('replace');
-var cssmin = require('cssmin');
-const terser = require('terser');
-
-const POUCHDB_CSS = resolvePath('docs/static/css/pouchdb.css');
-const POUCHDB_LESS = resolvePath('docs/src/less/pouchdb/pouchdb.less');
-
-process.chdir('docs');
+process.chdir('./');
 
 async function buildCSS() {
-  fs.mkdirSync(__dirname + '/../docs/static/css', { recursive:true });
-  const cmd = [ resolvePath('node_modules/less/bin/lessc'), POUCHDB_LESS ].join(' ');
-  const { stdout } = await exec(cmd);
-  const minifiedCss = cssmin(stdout);
-  fs.writeFileSync(POUCHDB_CSS, minifiedCss);
-  console.log('Updated:', POUCHDB_CSS);
+  await exec('npm run build:less');
+  console.log('=> Rebuilt CSS');
 }
 
 async function buildEleventy() {
-  await exec('npx @11ty/eleventy');
+  await exec('eleventy');
   await checkForUnprocessedCurlies();
   console.log('=> Rebuilt eleventy');
-
-  highlightEs6();
-  console.log('=> Highlighted ES6');
-
-  const srcPath = resolvePath('docs/src/code.js');
-  const targetPath = resolvePath('docs/_site/static/js/code.min.js');
-  const src = fs.readFileSync(srcPath, { encoding:'utf8' });
-  const mangle = { toplevel: true };
-  const output = { ascii_only: true };
-  const { code, error } = terser.minify(src, { mangle, output });
-  if (error) {
-    if (process.env.BUILD) {
-      throw error;
-    } else {
-      console.log(
-        `Javascript minification failed on line ${error.line} col ${error.col}:`,
-        error.message,
-      );
-    }
-  } else {
-    fs.writeFileSync(targetPath, code);
-    console.log('Minified javascript.');
-  }
 }
 
 async function checkForUnprocessedCurlies() {
@@ -77,34 +42,14 @@ async function checkForUnprocessedCurlies() {
   }
 }
 
-function highlightEs6() {
-  const path = resolvePath('docs/_site');
-
-  // TODO: this is a fragile and hacky way to get
-  // 'async' and 'await' to highlight correctly
-  // in blog posts & documentation.
-  replace({
-    regex: '<span class="nx">(await|async|of)</span>',
-    replacement: '<span class="kd">$1</span>',
-    paths: [path],
-    recursive: true
-  });
-}
-
-function onError(err) {
-  console.error(err);
-  process.exit(1);
-}
-
-function buildEverything() {
-  return Promise.resolve()
-    .then(buildCSS)
-    .then(buildEleventy)
-    .catch(onError);
-}
-
-function resolvePath(projectLocalPath) {
-  return Path.resolve(__dirname, '..', projectLocalPath);
+async function buildEverything() {
+  try {
+    await buildCSS();
+    await buildEleventy();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
 
 if (!process.env.BUILD) {
@@ -113,23 +58,23 @@ if (!process.env.BUILD) {
   const watchGlob = (path, fn) => globWatcher(path, () => fn().catch(console.log));
 
   // Simpler ways of blacklisting certain paths here would be very welcome.
-  fs.readdirSync('.')
+  fs.readdirSync('./docs')
     .forEach(path => {
       if (path === '_site') {
         return;
       }
 
-      if (fs.statSync(path).isDirectory()) {
-        watchGlob(`${path}/**`, buildEleventy);
+      if (fs.statSync(`./docs/${path}`).isDirectory()) {
+        watchGlob(`./docs/${path}/**`, buildEleventy);
       } else {
-        watchGlob(path, buildEleventy);
+        watchGlob(`./docs/${path}`, buildEleventy);
       }
     });
 
 
-  watchGlob('src/less/**', buildCSS);
+  watchGlob('./docs/src/less/**', buildCSS);
 
-  http_server.createServer({root: '_site', cache: '-1'}).listen(4000);
+  http_server.createServer({root: './docs/_site', cache: '-1'}).listen(4000);
   console.log('Server address: http://localhost:4000');
 }
 
